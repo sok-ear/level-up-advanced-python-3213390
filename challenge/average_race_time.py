f38c5d1ab7cb544944740637f3529824f9ea29c5
sok-ear <sok.ear@unimelb.edu.au>
2025-03-14 03:20:12 +0000

    average_race_time done

--- challenge/average_race_time.py
+++ challenge/average_race_time.py
@@ -3,6 +3,9 @@
 
 import re
 import datetime
+import statistics
+import math
+import datetime
 
 def get_data():
     """Return content from the 10k_racetimes.txt file"""
@@ -13,7 +16,15 @@ def get_data():
 def get_rhines_times():
     """Return a list of Jennifer Rhines' race times"""
     races = get_data()
-    pass
+    rhine_times = []
+
+    def get_times(line):
+        return re.findall(r'\d{2}:\S+', line)[0]
+    
+    for line in races.splitlines():
+        if "Jennifer Rhines" in line:
+            rhine_times.append(get_times(line))
+    return rhine_times
 
 def get_average():
     """Return Jennifer Rhines' average race time in the format:
@@ -22,4 +33,13 @@ def get_average():
        s corresponds to a seconds digit
        M corresponds to a milliseconds digit (no rounding, just the single digit)"""
     racetimes = get_rhines_times()
-    pass
+
+    sum = datetime.timedelta()
+    for racetime in racetimes:
+        time_value = racetime.split(":")
+        sum += datetime.timedelta(
+            minutes=int(time_value[0]), 
+            seconds=float(time_value[1]))
+
+    avg = sum/len(racetimes)
+    return f'{avg}'[2:-5]
