c2d38749c7b52b244553ce482b22c590410ce839
sok-ear <sok.ear@unimelb.edu.au>
2025-03-14 03:49:22 +0000

    pairwise_offset done

--- challenge/pairwise_offset.py
+++ challenge/pairwise_offset.py
@@ -1,2 +1,17 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+from itertools import tee, zip_longest, chain
+def pairwise_offset(sequence, offset=0, fillvalue="*"):
+    # convert to list
+    # list_sequence = []
+    # if not isinstance(sequence, list):
+    #     list_sequence = list(sequence)
+    # else:
+    #     list_sequence = sequence
+
+    # top_seq = list_sequence + ([fillvalue] * offset)
+    # bot_seq = ([fillvalue] * offset) + list_sequence
+
+    # return zip(top_seq,bot_seq)
+
+    # option 2
+    clone1, clone2 = tee(sequence, 2)
+    return zip_longest(clone1, chain(fillvalue * offset, clone2), fillvalue=fillvalue)
